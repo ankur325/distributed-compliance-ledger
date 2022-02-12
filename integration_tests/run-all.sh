42f6c463ec224da03adc12ed16d750508990162f
Jain, Ankur <Ankur_Jain@comcast.com>
2022-02-12 12:00:32 -0500

    wip

--- integration_tests/run-all.sh
+++ integration_tests/run-all.sh
@@ -102,53 +102,12 @@ make image &>${DETAILED_OUTPUT_TARGET}
 
 cleanup_pool
 
-# Cli shell tests
-if [[ $TESTS_TO_RUN =~ "all" || $TESTS_TO_RUN =~ "cli" ]]; then
-  CLI_SHELL_TESTS=$(find integration_tests/cli -type f -name '*.sh' -not -name "common.sh")
 
-  for CLI_SHELL_TEST in ${CLI_SHELL_TESTS}; do
-    init_pool
 
-    log "*****************************************************************************************"
-    log "Running $CLI_SHELL_TEST"
-    log "*****************************************************************************************"
-
-    if bash "$CLI_SHELL_TEST" &>${DETAILED_OUTPUT_TARGET}; then
-      log "$CLI_SHELL_TEST finished successfully"
-    else
-      log "$CLI_SHELL_TEST failed"
-      exit 1
-    fi
-
-    cleanup_pool
-  done
-fi
-
-# Light Client Proxy Cli shell tests
-if [[ $TESTS_TO_RUN =~ "all" || $TESTS_TO_RUN =~ "light" ]]; then
-  CLI_SHELL_TESTS=$(find integration_tests/light_client_proxy -type f -name '*.sh' -not -name "common.sh")
-
-  for CLI_SHELL_TEST in ${CLI_SHELL_TESTS}; do
-    init_pool 
-
-    log "*****************************************************************************************"
-    log "Running $CLI_SHELL_TEST"
-    log "*****************************************************************************************"
-    
-    if bash "$CLI_SHELL_TEST" &>${DETAILED_OUTPUT_TARGET}; then
-      log "$CLI_SHELL_TEST finished successfully"
-    else
-      log "$CLI_SHELL_TEST failed"
-      exit 1
-    fi
-
-    cleanup_pool
-  done
-fi
 
 # Go rest tests
 if [[ $TESTS_TO_RUN =~ "all" || $TESTS_TO_RUN =~ "rest" ]]; then
-  GO_REST_TESTS="$(find integration_tests/grpc_rest -type f -name '*_test.go')"
+  GO_REST_TESTS="$(find integration_tests/grpc_rest/compliance -type f -name '*_test.go')"
 
   for GO_REST_TEST in ${GO_REST_TESTS}; do
     init_pool
